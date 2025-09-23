4852f7d625e9474317c7b0a6fb5fd4d84113555e
Pablo <pgonzalezg12@educantabria.es>
2025-09-23 20:14:43 +0200

    Ejercicios de Logica

--- Desarrollo Web en Entorno Cliente/UT1/Ejercicios/Ejercicios_Logica/1.js
+++ Desarrollo Web en Entorno Cliente/UT1/Ejercicios/Ejercicios_Logica/1.js
@@ -3,4 +3,21 @@ let cadena1 = "Pablo";
 let cadena2 = "González";
 let numero1 = 19;
 let numero2 = 2006;
-alert(cadena1);
+
+/* 1-  Muestra en un alert las variables introducidas con formato clave valor en donde los
+valores cadena aparezcan envueltos entre comillas dobles y los valores numéricos
+entre comillas simples.*/
+alert('nombre ="'+cadena1+'"\n'
+    +'apellido ="'+cadena2+'"\n'
+    +'edad ="'+numero1+'"\n'
+    +'año de nacimiento ="'+numero2+'"'
+);
+
+/* 2- Muestra en un alert tu nombre y apellidos separados por un salto de línea. */ 
+alert(cadena1+'\n'+cadena2);
+
+/* 3- Muestra en un alert la suma de las variables edad y año de nacimiento*/
+alert(numero1+numero2);
+
+/* 4- Muestra en un alert la suma de todas las variables. */
+alert(cadena1+cadena2+numero1+numero2);
