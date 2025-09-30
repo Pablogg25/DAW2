dd408f2b2dd018aca2eeb56dc7dc86f734c0b286
Pablo <pgonzalezg12@educantabria.es>
2025-09-30 19:47:26 +0200

    EJ2

---
+++ Desarrollo Web en Entorno Cliente/UT1/Ejercicios/Ejercicios_Refuerzo/2.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+    let fibonacci = [];
+    let contador = 0;
+    if (n > 0) fibonacci.push(0);
+    if (n > 1) fibonacci.push(1);
+    contador = fibonacci.length;
+    while (contador < n) {
+        fibonacci.push(fibonacci[contador - 1] + fibonacci[contador - 2]);
+        contador++;
+    }
+    return fibonacci;
+}
+
+console.log(fibonacci(10));
