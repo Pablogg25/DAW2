2813e2a1290d0b41950173d58e413ef135bdbcf1
Pablo <pgonzalezg12@educantabria.es>
2025-10-01 20:03:27 +0200

    Index Ejercicios PHP

--- Desarrollo web en Entorno Servidor/UT3/Ejercicios/index.php
+++ Desarrollo web en Entorno Servidor/UT3/Ejercicios/index.php
@@ -8,7 +8,22 @@
 </head>
 
 <body>
-
+    <?php
+    require "Ej11.php";
+    // Ejemplo de uso:
+    /*$texto = "Hola mundo, hola universo, hola galaxia";
+    $palabra = "hola";
+    $posicion = encontrarUltimaPosicion($texto, $palabra);
+    echo "La última aparición de '$palabra' está en la posición: $posicion";
+    */
+    // Ejemplo de uso:
+    $texto1 = "Hola";
+    $texto2 = "hola";
+    if (sonCadenasIguales($texto1, $texto2)) {
+        echo "Las cadenas son exactamente iguales.";
+    } else {
+        echo "Las cadenas son diferentes.";
+    } ?>
 </body>
 
 </html>
