965bfc3df118ac317ba2d8587015a23e9c3bb73a
Pablo <pgonzalezg12@educantabria.es>
2025-10-01 20:11:13 +0200

    EJ12 PHP

--- Desarrollo web en Entorno Servidor/UT3/Ejercicios/index.php
+++ Desarrollo web en Entorno Servidor/UT3/Ejercicios/index.php
@@ -9,21 +9,29 @@
 
 <body>
     <?php
-    require "Ej11.php";
+    require "Ej12.php";
     // Ejemplo de uso:
     /*$texto = "Hola mundo, hola universo, hola galaxia";
     $palabra = "hola";
     $posicion = encontrarUltimaPosicion($texto, $palabra);
     echo "La última aparición de '$palabra' está en la posición: $posicion";
     */
-    // Ejemplo de uso:
+    /*// Ejemplo de uso 11:
     $texto1 = "Hola";
     $texto2 = "hola";
     if (sonCadenasIguales($texto1, $texto2)) {
         echo "Las cadenas son exactamente iguales.";
     } else {
         echo "Las cadenas son diferentes.";
-    } ?>
+    } */
+    // Ejemplo de uso de sonCadenasIguales:
+    $cadena = "Hola mundo, esto es un ejemplo";
+    $inicio = 5;
+    $longitud = 10;
+    $fragmento = devolverFragmento($cadena, $inicio, $longitud);
+    echo "El fragmento extraído es: '$fragmento'";
+    ?>
+
 </body>
 
 </html>
