5760537b489a625473a15e2d05d67398b29efd2d
Pablo <pgonzalezg12@educantabria.es>
2025-10-07 18:17:25 +0200

    Ejercicio 9 JAVASCRIPT

---
+++ Desarrollo Web en Entorno Cliente/UT1/Ejercicios/Ejercicios_Refuerzo/9.js
@@ -0,0 +1,29 @@
+function Marco(frase) {
+    let palabras = frase.split();
+
+    let maxLong = 0;
+    for (let i = 0; i < palabras.length; i++) {
+        if (palabras[i].length > maxLong) {
+            maxLong = palabras[i].length;
+        }
+    }
+
+    let borde = "";
+    for (let i = 0; i < maxLong + 4; i++) {
+        borde += "*";
+    }
+    console.log(borde);
+
+    for (let i = 0; i < palabras.length; i++) {
+        let palabra = palabras[i];
+        let espacios = "";
+        for (let j = palabras.length; j < maxLong; j++) {
+            espacios += " ";
+        }
+        console.log("* " + palabra + espacios + " *")
+    }
+    console.log(borde);
+}
+
+//Ejemplo
+Marco("¿que le parece el reto?");
