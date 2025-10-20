55d326122463307f80a0a38ba1d9632501286fc4
Pablo <pgonzalezg12@educantabria.es>
2025-10-20 14:32:43 +0200

    Repetimos Ejercicios UT2 PHP

---
+++ DWES/UT2/Ejercicios_Repetidos/EJ2.php
@@ -0,0 +1,7 @@
+<?php
+$variable1=16;
+$variable2=20;
+$variable3=53;
+
+echo 'Hola '.$variable1.' Adios '.$variable2.' Que tal '.$variable3;
+?>
---
+++ DWES/UT2/Ejercicios_Repetidos/EJ3.php
@@ -0,0 +1,7 @@
+<?php
+$nombre = 'Pablo';
+$apellido1 ='González';
+$apellido2 = 'García';
+
+echo 'Hola mi nombre es '.$nombre.' '.$apellido1.' '.$apellido2;
+?>
