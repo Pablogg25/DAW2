cf155c1b2eba793ece631029e982eb59cd0fc629
Pablo González García <pgonzalezg12@educantabria.es>
2026-01-08 20:15:29 +0100

    Add Menu and Contenido components to App
    Created new Menu and Contenido components and integrated them into App.jsx. Commented out unused code and replaced the default content with the new components, passing a 'mensaje' prop to Contenido.

--- DWEC/UT4 - React/Ejercicios/Prueba-1/prueba/src/App.jsx
+++ DWEC/UT4 - React/Ejercicios/Prueba-1/prueba/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-
+import Menu from './components/Menu.jsx'
+import Contenido from './components/Contenido.jsx'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <Menu></Menu>
+    <Contenido mensaje='Mola Mazo'/>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +30,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ DWEC/UT4 - React/Ejercicios/Prueba-1/prueba/src/components/Contenido.jsx
@@ -0,0 +1,9 @@
+// type,props,children
+function Contenido({mensaje="valor por defecto"}){
+return(
+<>
+<p>{mensaje}</p>
+</>
+);
+}
+export default Contenido;
