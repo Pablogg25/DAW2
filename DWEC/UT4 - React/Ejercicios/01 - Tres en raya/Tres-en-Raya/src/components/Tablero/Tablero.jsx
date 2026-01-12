6e34281c3adc06d426d2d80467ca15cdca69060c
Pablo González García <pgonzalezg12@educantabria.es>
2026-01-12 19:39:43 +0100

    Refactorizar para usar el componente Tablero y eliminar la aplicación
    Reemplaza el componente de la aplicación con un nuevo componente de Tablero para la interfaz de usuario principal. Elimina App.jsx y su CSS y actualiza main.jsx para representar Tablero desde la nueva ubicación.

---
+++ DWEC/UT4 - React/Ejercicios/01 - Tres en raya/Tres-en-Raya/src/components/Tablero/Tablero.jsx
@@ -0,0 +1,21 @@
+function tablero(params) {
+  return (
+    <>
+      <div id="info"></div>
+      <button>Comenzar</button>
+      <br />
+      <div id="1"></div>
+      <div id="2"></div>
+      <div id="3"></div>
+      <br />
+      <div id="4"></div>
+      <div id="5"></div>
+      <div id="6"></div>
+      <br />
+      <div id="7"></div>
+      <div id="8"></div>
+      <div id="9"></div>
+    </>
+  );
+}
+export default tablero;
