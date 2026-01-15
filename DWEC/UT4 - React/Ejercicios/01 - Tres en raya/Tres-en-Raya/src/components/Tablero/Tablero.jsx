57f3bcd163195ce983398c092f7b6be9c5be3585
Pablo <pgonzalezg12@educantabria.es>
2026-01-15 19:44:58 +0100

    Agregar estilos y refactorizar el diseño de componentes de Tablero
    Creé Tablero.css para diseñar tableros y celdas. Tablero.jsx refactorizado para usar nombres de clases semánticas y un diseño de caja flexible para la cuadrícula de tres en raya. Index.css actualizado con diseño general y estilos de botones para mejorar la apariencia.

--- DWEC/UT4 - React/Ejercicios/01 - Tres en raya/Tres-en-Raya/src/components/Tablero/Tablero.jsx
+++ DWEC/UT4 - React/Ejercicios/01 - Tres en raya/Tres-en-Raya/src/components/Tablero/Tablero.jsx
@@ -1,21 +1,29 @@
-function tablero(params) {
+import "./Tablero.css";
+function Tablero() {
   return (
     <>
       <div id="info"></div>
       <button>Comenzar</button>
-      <br />
-      <div id="1"></div>
-      <div id="2"></div>
-      <div id="3"></div>
-      <br />
-      <div id="4"></div>
-      <div id="5"></div>
-      <div id="6"></div>
-      <br />
-      <div id="7"></div>
-      <div id="8"></div>
-      <div id="9"></div>
+
+      <div className="fila">
+        <div className="casilla"></div>
+        <div className="casilla"></div>
+        <div className="casilla"></div>
+      </div>
+
+      <div className="fila">
+        <div className="casilla"></div>
+        <div className="casilla"></div>
+        <div className="casilla"></div>
+      </div>
+
+      <div className="fila">
+        <div className="casilla"></div>
+        <div className="casilla"></div>
+        <div className="casilla"></div>
+      </div>
     </>
   );
 }
-export default tablero;
+
+export default Tablero;
