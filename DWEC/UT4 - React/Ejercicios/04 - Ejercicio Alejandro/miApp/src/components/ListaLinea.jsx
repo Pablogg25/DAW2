3cbb8349e0cffcb7e9c92fdf16566baf458a66c7
Pablo <pgonzalezg12@educantabria.es>
2026-01-22 20:17:14 +0100

    Update ListaLinea.jsx

--- DWEC/UT4 - React/Ejercicios/04 - Ejercicio Alejandro/miApp/src/components/ListaLinea.jsx
+++ DWEC/UT4 - React/Ejercicios/04 - Ejercicio Alejandro/miApp/src/components/ListaLinea.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 function ListaLinea({ modulo }) {
   const { id = 0, nombre = "", horas = 0 } = modulo;
   const navegar = useNavigate();
