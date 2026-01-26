bd3f7e1a9fa5811253cf102023d8e4ac047b92a2
Pablo <pgonzalezg12@educantabria.es>
2026-01-26 20:15:19 +0100

    Update AppEnrutador.jsx

--- DWEC/UT4 - React/Ejercicios/05 - Concesionario/concesionario/src/routes/AppEnrutador.jsx
+++ DWEC/UT4 - React/Ejercicios/05 - Concesionario/concesionario/src/routes/AppEnrutador.jsx
@@ -3,6 +3,7 @@ import AppLayout from "../components/AppLayout/AppLayout.jsx";
 import HomePage from "../pages/HomePage.jsx";
 import MantenimientoPage from "../pages/MantenimientoPage.jsx";
 import LoginPage from "../pages/LoginPage.jsx";
+import PropsCochePage from "../pages/PropsCochePage";
 function AppEnrutador() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function AppEnrutador() {
           <Route index element={<HomePage />} />
           <Route path="mantenimiento" element={<MantenimientoPage />} />
           <Route path="login" element={<LoginPage />} />
+          <Route path="props/:id" element={<PropsCochePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
