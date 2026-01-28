0345e00771be16e1b45b96b53f039c691c68b316
Pablo <pgonzalezg12@educantabria.es>
2026-01-28 15:36:20 +0100

    JS - UT4 - Concesionario
    Añadimos fichas de coches de forma aleatoria a la pagina de inicio de nuestro taller

--- DWEC/UT4 - React/Ejercicios/05 - Concesionario/concesionario/src/pages/HomePage.jsx
+++ DWEC/UT4 - React/Ejercicios/05 - Concesionario/concesionario/src/pages/HomePage.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+import Negocio from "../core/negocio";
+import "./HomePage.css";
+
 function HomePage() {
+  const [coches, setCoches] = useState([]);
+  const [destacados, setDestacados] = useState([]);
+
+  useEffect(() => {
+    async function cargar() {
+      const lista = await Negocio.obtenerCoches();
+      setCoches(lista);
+
+      const aleatorios = [...lista].sort(() => Math.random() - 0.5).slice(0, 3);
+      setDestacados(aleatorios);
+    }
+    cargar();
+  }, []);
+
   return (
     <>
-      <p>HomePage</p>
+      <h2>Inicio</h2>
+      <div className="lista-coches">
+        {destacados.map((coche) => (
+          <div key={coche.id} className="coche-card">
+            <h4>
+              {coche.marca} {coche.modelo}
+            </h4>
+            <p>Año: {coche.anoo}</p>
+            <p>Precio: {coche.precio}</p>
+            <p>Estado: {coche.estado}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
